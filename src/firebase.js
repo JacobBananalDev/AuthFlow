30b54d2f47744b6a2e9d5d011ef070150a79e916
JbanzCodingLabz <jacob.r.bananal@outlook.com>
2024-10-11 16:34:22 -0700

    AuthFlow - Added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDoZ-pxMQUzzIEkALCU3p3-LG4EOSwTEV4",
+  authDomain: "authflow-bc06d.firebaseapp.com",
+  projectId: "authflow-bc06d",
+  storageBucket: "authflow-bc06d.appspot.com",
+  messagingSenderId: "1092644429693",
+  appId: "1:1092644429693:web:3d155e2ccd39f9414b7614",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
